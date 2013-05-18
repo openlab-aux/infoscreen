9ffc82fa4dc5a9608ddb9a723801ca540b37ac3d
grindhold <grindhold@gmx.net>
2013-05-18 04:37:58 +0200

    infoscreen laedt infopages aus dem ordner "pages"

--- infoscreen.py
+++ infoscreen.py
@@ -57,21 +57,30 @@ class InfoScreen(object):
         yellowtext_height = 5
         yellowtext_width = 42
         self.yellowtextwin = curses.newwin(yellowtext_height, yellowtext_width, yellowtext_begin_y, yellowtext_begin_x)
+        self.current_page = 0
+        self.pages = []
+
         self.update_data()
 
     def update_data(self):
+        if not os.path.exists("./pages/"):
+            os.mkdir("pages")
         self.yellowtext = json.loads(open("yellowtext.json","r").read())
+        self.pages = [open("pages/"+f,"r").read() for f in os.listdir("pages")]
 
     def render_yellowtext(self):
-        yellowtext = self.yellowtext[randint(0, len(self.yellowtext)-1)]
-        if len(yellowtext) > 42:
-            arr = []
-            while len(yellowtext) >1:
-                arr.append(yellowtext[:41])
-                yellowtext=yellowtext[41:]
-            arr.append(yellowtext[:41]+" "*(42-len(yellowtext[:41])))
-            return "\n".join(arr)
-        return yellowtext+" "*(42-len(yellowtext))
+        if len(self.yellowtext) > 0:
+            yellowtext = self.yellowtext[randint(0, len(self.yellowtext)-1)]
+            if len(yellowtext) > 42:
+                arr = []
+                while len(yellowtext) >1:
+                    arr.append(yellowtext[:41])
+                    yellowtext=yellowtext[41:]
+                arr.append(yellowtext[:41]+" "*(42-len(yellowtext[:41])))
+                return "\n".join(arr)
+            return yellowtext+" "*(42-len(yellowtext))
+        else:
+            return "OHAI!"
     
     def fillinfowin(self):
         string = ""
@@ -80,14 +89,20 @@ class InfoScreen(object):
         return string+" "*(self.info_width-1)
 
     def drawscreen(self):
-        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_GREEN)
+        curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
         curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
         curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED)
 
         self.logowin.addstr(0,0, LOGO, curses.color_pair(1))
         self.yellowtextwin.addstr(0,0, self.render_yellowtext(), curses.color_pair(2))
         self.infowin.addstr(0,0, self.fillinfowin(), curses.color_pair(3))
-        self.infowin.addstr(1,1, "benis", curses.color_pair(3))
+        self.current_page += 1
+        if self.current_page >= len(self.pages):
+            self.current_page = 0
+        if len(self.pages) > 0:
+            self.infowin.addstr(1,1, self.pages[self.current_page], curses.color_pair(3))
+        else:
+            self.infowin.addstr(1,1, "Gehen sie weiter, hier gibt es nichts zu sehen.", curses.color_pair(3))
         self.logowin.refresh()
         self.yellowtextwin.refresh()
         self.infowin.refresh()
